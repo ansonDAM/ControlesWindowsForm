36983aff7245af12d64cff97912566709bc96534
anson <3823613@alu.murciaeduca.es>
2022-09-30 19:16:57 +0200

    Práctica 10 acabada.

--- Pr0210/Form1.Designer.cs
+++ Pr0210/Form1.Designer.cs
@@ -31,7 +31,9 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -40,10 +42,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(67, 21);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(664, 331);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Size = new System.Drawing.Size(800, 331);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -62,17 +63,31 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.trackBar1.Location = new System.Drawing.Point(56, 359);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(669, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickFrequency = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +96,6 @@
         private PictureBox pictureBox1;
         private OpenFileDialog openFileDialog1;
         private Button button1;
+        private TrackBar trackBar1;
     }
 }
--- Pr0210/Form1.cs
+++ Pr0210/Form1.cs
@@ -2,9 +2,9 @@ namespace Pr0210
 {
     public partial class Form1 : Form
     {
-        Bitmap imagen;
-        int ancho;
-        int largo;
+        private Bitmap imagen;
+        int width;
+        int heigth;
 
         public Form1()
         {
@@ -17,7 +17,15 @@ namespace Pr0210
             if (seleccion.Equals(DialogResult.OK)) {
                 imagen = new Bitmap(openFileDialog1.FileName);
                 pictureBox1.Image = imagen;
+                width = imagen.Width;
+                heigth = imagen.Height;
             }  
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Size newSize = new Size(width+trackBar1.Value, heigth+trackBar1.Value);
+            pictureBox1.Image = new Bitmap(imagen, newSize);
         }
     }
 }
