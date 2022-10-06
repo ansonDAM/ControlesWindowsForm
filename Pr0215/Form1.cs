b0d193ac99b5095e381c4868ec87b9a2db6d3721
anson <3823613@alu.murciaeduca.es>
2022-10-06 16:21:30 +0200

    Práctica 15 terminada.

---
+++ Pr0215/Form1.cs
@@ -0,0 +1,16 @@
+namespace Pr0215
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(textBox1.Text);
+            form2.ShowDialog();
+        }
+    }
+}
